3aa296623995922ab67ddb65207837e6441dc2a5
Nikola Stefanovski <nikoding32@gmail.com>
2023-06-15 20:30:24 +0200

    Types and classes defined

---
+++ public/app.js
@@ -0,0 +1,25 @@
+"use strict";
+// classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes €${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('Floki', 'work on a website', 250);
+const invTwo = new Invoice('Max', 'work on a website', 240);
+let invoices = [];
+// inputs
+const form = document.querySelector('.new-item-form');
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
---
+++ src/app.ts
@@ -0,0 +1,39 @@
+// classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number){
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+  format() {
+    return `${this.client} owes €${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice('Floki', 'work on a website', 250);
+const invTwo = new Invoice('Max', 'work on a website', 240);
+
+let invoices: Invoice[] = [];
+
+// inputs
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  console.log(
+    type.value,
+    tofrom.value,
+    details.value,
+    amount.valueAsNumber
+  )
+})

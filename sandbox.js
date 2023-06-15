cb7092419b046115e5a66a17674916ce946f6897
Nikola Stefanovski <nikoding32@gmail.com>
2023-06-15 13:20:39 +0200

    project setup

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,1 @@
 var character = 'Floki';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
--- sandbox.ts
+++ sandbox.ts
@@ -1,11 +1,1 @@
 const character = 'Floki';
-
-console.log(character);
-
-const inputs = document.querySelectorAll('input');
-
-console.log(inputs);
-
-inputs.forEach(input => {
-  console.log(input);
-})

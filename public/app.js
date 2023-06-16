834b1b6464c7369f6dc699467c98c2626f362d6f
Nikola Stefanovski <nikoding32@gmail.com>
2023-06-16 22:55:27 +0200

    added list templates and rendering list in DOM

--- public/app.js
+++ public/app.js
@@ -1,19 +1,36 @@
 import { Invoice } from './classes/invoice.js';
 import { Payment } from './classes/payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 // inputs
 const form = document.querySelector('.new-item-form');
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+//list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
+//Generics
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'Floki', age: 40 });
+const docThree = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'Floki' }
+};
